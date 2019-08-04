6083ac2412f20b173d2b704b75af954f8ddcd578
Yadav <rayadav@internal.tavisca.com>
2019-08-04 12:45:13 +0530

    Handles the logs using LogManager

--- src/main/java/LogHandler.java
+++ src/main/java/LogHandler.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class LogHandler {
+    public static Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
 }
