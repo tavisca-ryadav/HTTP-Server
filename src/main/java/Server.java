7f4ebeaa546ace841fdff681343e917937ba9a20
Yadav <rayadav@internal.tavisca.com>
2019-08-04 10:40:55 +0530

    server start class implemented

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,73 +1,38 @@
-
-import javax.annotation.processing.FilerException;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Server {
-    private Socket socket = null;
-    private ServerSocket server = null;
-    private BufferedInputStream bufferedInputStream = null;
-
-    public Server(int port){
+    public  ServerSocket server;
+    public ClientHandler handleClient;
+    public static Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
+    public boolean running=false;
+    public boolean serverSuccesfullyStarted=false;
+    public void startServer(int port){
         try {
+            logger.setLevel(Level.INFO);
             server = new ServerSocket(port);
-            System.out.println("Server Started");
-            System.out.println("waiting for client");
-            socket = server.accept();
-            System.out.println("Client accepted");
+            serverSuccesfullyStarted=true;
+            running = true;
+            logger.log(Level.INFO,"Server Started\nwaiting for clients");
+            stopServer();
 
-            File file = parseRequest(socket);
-            handleResponse(socket,file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private File parseRequest(Socket socket) {
-        String fileName = "";
-        try {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-            byte[] byteArray = new byte[bufferedInputStream.available()];
-            bufferedInputStream.read(byteArray);
-            String content = new String(byteArray);
-            String[] split = content.split(" ");
-            fileName = split[1];
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new File("Files/"+fileName);
-
-    }
-
-
-
-    private void handleResponse(Socket socket,File file) {
-        PrintWriter out = null;
-        try {
-            out = new PrintWriter(socket.getOutputStream());
-            out.print("HTTP/1.1 200 \r\n"); // Version & status code
-            out.print("Content-Type:html\r\n"); // The type of data
-            out.print("Connection: close\r\n"); // Will close stream
-            out.print("\r\n"); // End of headers
-            if(file.exists()){
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                while(bufferedReader.readLine()!=null)
-                out.print(bufferedReader.readLine());
+           while(running){
+                Socket socket = server.accept();
+                handleClient = new ClientHandler(socket);
+                Thread thread = new Thread((handleClient));
+                thread.start();
             }
-            else
-                out.print("<html><body><h>Error 404 File not Found</h></body></html>");
 
-            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    public static void main(String[] args){
-        Server server =new Server(5000);
+    public void stopServer(){
+        running = false;
     }
 }
