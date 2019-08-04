e349b4c040a95c56e641550a95dccf0e69629835
Yadav <rayadav@internal.tavisca.com>
2019-08-04 12:43:36 +0530

    Http client request responder class implemented

--- src/main/java/Responder.java
+++ src/main/java/Responder.java
@@ -1,17 +1,39 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.logging.Level;
 
-public class Respond {
-    public void handle(Socket socket, String fileContent,String fileType) {
-        PrintWriter out = null;
+public class Responder {
+    private File file;
+    private Socket socket;
+    private String fileType;
+
+    public Responder(File file, Socket socket) {
+        this.file = file;
+        this.socket = socket;
+    }
+
+    public void handle() {
+        LogHandler.logger.log(Level.INFO,file.getName()+" has been loaded");
+        RequestParser requestParser = new RequestParser();
+        fileType = requestParser.extractTypeOfFile(file.getName());
+
         try {
-            out = new PrintWriter(socket.getOutputStream());
-            out.print("HTTP/1.1 200 \r\nContent-Type:"+fileType+"\r\nConnection: close\r\n\r\n");
-            out.print(fileContent);
-            out.close();
-        } catch (IOException e) {
+            java.io.File file1 = new java.io.File(file.getPath());
+            FileInputStream fileInputStream = new FileInputStream(file1);
+            int fileLength = (int) file1.length();
+            byte[] fileInBytes = new byte[fileLength];
+            fileInputStream.read(fileInBytes);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+            bufferedOutputStream.write(("HTTP/1.1 " + 200 + " " + " OK " + "\r\n").getBytes());
+            bufferedOutputStream.write(("Content-Type:" + fileType + "\r\n").getBytes());
+            bufferedOutputStream.write(("Content-Length: " + fileLength + "\r\n").getBytes());
+            bufferedOutputStream.write("\r\n".getBytes());
+            bufferedOutputStream.write(fileInBytes, 0, fileLength);
+            bufferedOutputStream.flush();
+
+        } catch (Exception e) {
+            LogHandler.logger.log(Level.INFO,"Exception in file : " + file.getPath());
             e.printStackTrace();
         }
-
     }
 }
