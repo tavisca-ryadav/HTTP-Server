c0bd6c9eecd64a3e07ecaafbcc27835c9104eb99
Yadav <rayadav@internal.tavisca.com>
2019-08-04 12:42:39 +0530

    Method added for Getting request from Client

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.function.Supplier;
+import java.util.logging.Level;
 
 public class Request {
+    public String recieveRequest(Socket socket) {
+        String content="";
+        try {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
+            byte[] byteArray = new byte[bufferedInputStream.available()];
+            bufferedInputStream.read(byteArray);
+            content = new String(byteArray);
+        } catch (IOException e) {
+            LogHandler.logger.log(Level.INFO, (Supplier<String>) e);
+        }
+        return content;
+    }
 }
