173ccb3caecceebfe7527af50bed8bf836f909d3
Yadav <rayadav@internal.tavisca.com>
2019-08-04 12:44:31 +0530

    Client Handler Implements MultiThreading

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,28 +1,27 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
 
-public class HandleClient implements Runnable {
+public class ClientHandler implements Runnable {
     private Socket socket=null;
     private Request request;
     private Responder responder;
-    private FileReader fileReader;
     private RequestParser requestParser;
-    public HandleClient(Socket socket){
+    public ClientHandler(Socket socket){
        this.socket = socket;
        request = new Request();
-       fileReader = new FileReader();
        requestParser = new RequestParser();
     }
 
     @Override
     public void run() {
         String recievedContent = request.recieveRequest(socket);
+        LogHandler.logger.log(Level.INFO,recievedContent);
         String filePath = requestParser.extractFilePath(recievedContent);
-
         File file = new File("Files/" + filePath);
         if (!file.exists()) {
-            System.out.println(filePath + " not found");
+            LogHandler.logger.log(Level.INFO,filePath + " not found");
             file = new File("Files/error.html");
         }
 
