4d1e484eba214a4eb88158d9e4db7ae6afd90818
Yadav <rayadav@internal.tavisca.com>
2019-08-04 12:45:46 +0530

    Server Implemented

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,23 +2,20 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class Server {
     public  ServerSocket server;
     public ClientHandler handleClient;
-    public static Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
     public boolean running=false;
     public boolean serverSuccesfullyStarted=false;
     public void startServer(int port){
         try {
-            logger.setLevel(Level.INFO);
+            LogHandler.logger.setLevel(Level.INFO);
             server = new ServerSocket(port);
             serverSuccesfullyStarted=true;
             running = true;
-            logger.log(Level.INFO,"Server Started\nwaiting for clients");
-            stopServer();
+            LogHandler.logger.log(Level.INFO,"Server Started\nwaiting for clients");
+            stopServer();//Stopping the server for running test cases
 
            while(running){
                 Socket socket = server.accept();
@@ -34,5 +31,11 @@ public class Server {
 
     public void stopServer(){
         running = false;
+        LogHandler.logger.log(Level.INFO,"Server Stopped");
+    }
+
+    public static void main(String[] args){
+        Server server = new Server();
+        server.startServer(5000);
     }
 }
