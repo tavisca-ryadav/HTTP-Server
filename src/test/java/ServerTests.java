ea4f6e072f7d3a35d969e36f1c40de4ac3e7a7a4
Yadav <rayadav@internal.tavisca.com>
2019-08-04 12:40:23 +0530

    Parser Tests are added

--- src/test/java/ServerTests.java
+++ src/test/java/ServerTests.java
@@ -12,10 +12,31 @@ public class ServerTests {
     }
 
     @Test
-    public void ServerStarted(){
-       server.stopServer();
+    public void TestServer(){
         Assert.assertEquals(true,server.serverSuccesfullyStarted);
     }
+
+    @Test
+    public void TestExtractFilePathFromRecievedHeaderRequest(){
+       RequestParser requestParser = new RequestParser();
+       String requestHeader = "GET /web/index.html HTTP/1.1\n" +
+               "Host: localhost:5000\n" +
+               "Connection: keep-alive\n" +
+               "Cache-Control: max-age=0\n";
+       Assert.assertEquals("/web/index.html",requestParser.extractFilePath(requestHeader));
+    }
+
+    @Test
+    public void TestFileExtension(){
+       RequestParser requestParser = new RequestParser();
+       Assert.assertEquals("html",requestParser.extractTypeOfFile("File/web/index.html"));
+       Assert.assertEquals("css",requestParser.extractTypeOfFile("File/web/bootstrap.css"));
+       Assert.assertEquals("png",requestParser.extractTypeOfFile("Testimonial-1.png"));
+       Assert.assertEquals("jpg",requestParser.extractTypeOfFile("File/web/images/screenshot.jpg"));
+    }
+
+    @Test
+
 
    @After
     public void after(){
